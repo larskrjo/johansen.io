1910f747b9450e2550dce5bda0f3fed5ced2ba49
Lars K. Johansen <larskrjo@gmail.com>
2015-02-16 20:10:46 -0800

    Initial commit

---
+++ database/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+username=null
+password=null
+
+while read line
+  do
+    username=$line
+  done < $1
+
+while read line
+  do
+    password=$line
+  done < $2
+
+mysql -h localhost --user=$username --password=$password < ~/larskristian.net/database/migration/prod/baseline.sql
+mysql -h localhost --user=$username --password=$password < ~/larskristian.net/database/migration/test/baseline.sql
